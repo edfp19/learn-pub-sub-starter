879149fba14d0241247ae8ce28032d296e474e6a
Ever <78941506+edfp19@users.noreply.github.com>
2026-09-11 16:51:38 +0000

    updating the rest

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+	const amqpURI = "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(amqpURI)
+	if err != nil {
+		log.Fatalf("failed")
+	}
+	defer conn.Close()
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatalf("failed to welcome client")
+	}
+
+	_, _, err = pubsub.DeclareAndBind(conn, routing.ExchangePerilDirect, "pause"+"."+username, routing.PauseKey, pubsub.TransientQueue)
+	if err != nil {
+		log.Fatalf("failed to declare and bind queue")
+	}
+	fmt.Printf("Welcome, %s!\n", username)
+
+	gameState := gamelogic.NewGameState(username)
+
+	for {
+		input := gamelogic.GetInput()
+		if len(input) == 0 {
+			fmt.Println("No input detected. Please enter a command.")
+			continue
+		}
+		command := input[0]
+		switch command {
+			case "spawn":
+				
+
+		
+	}
+
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	<-signalChan
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	fmt.Println("Starting Peril client...")
+
+	const amqpURI = "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(amqpURI)
+	if err != nil {
+		log.Fatalf("failed to connect to RabbitMQ: %v", err)
+	}
+	defer conn.Close()
+	channel, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("failed to open channel: %v", err)
+	}
+
+	fmt.Println("Connection successful")
+	gamelogic.PrintServerHelp()
+
+outerLoop:
+	for {
+		arr := gamelogic.GetInput()
+		if len(arr) == 0 {
+			fmt.Println("No input detected. Please enter a command.")
+			continue
+		}
+		firstWord := arr[0]
+
+		switch firstWord {
+		case "pause":
+			fmt.Println("Sending a pause message")
+			err := pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+				IsPaused: true,
+			})
+			if err != nil {
+				log.Fatalf("failed to send pause message: %v", err)
+			}
+
+		case "resume":
+			fmt.Println("Sending a resume message")
+			err := pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+				IsPaused: false,
+			})
+			if err != nil {
+				log.Fatalf("failed to send resume message: %v", err)
+			}
+
+		case "quit":
+			fmt.Println("Quitting the server")
+			break outerLoop
+
+		default:
+			fmt.Println("Can't understand command")
+		}
+
+		fmt.Println("Shutting down")
+
+	}
 }
